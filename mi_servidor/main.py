26b89c60817c91461ba52f5667602772db29b565
brunoricciweb <bruno.ricci@bue.edu.ar>
2024-06-13 23:22:19 +0000

    backup

--- mi_servidor/main.py
+++ mi_servidor/main.py
@@ -7,9 +7,21 @@ app = FastAPI()
 async def root():
     return {"message": "Hola! servidor de prueba de la clase de Esp. en desarrollo Web!"}
 
-
+@app.get("/search")
+async def root(busqueda:str):
+    return f"La búsqueda es: '{busqueda}'"
 
 @app.get("/saludar/{nombre}")
 async def root(nombre:str):
     return f'Hola, {nombre}, ¿Cómo estás?. \n Este es el servidor de Bruno.'
 
+@app.get("/obtenermayor")
+async def root(num1:int, num2:int):
+    numeroMayor=None
+    if num1 > num2: 
+        numeroMayor = num1
+    elif num1 < num2:
+        numeroMayor = num2
+    else:
+        return 'Los números son iguales.'    
+    return f'El número mayor es {numeroMayor}'
