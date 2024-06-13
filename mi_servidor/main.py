be455d8a7067633e199640fbb44b4bf157f561e1
brunoricciweb <bruno.ricci@bue.edu.ar>
2024-06-13 23:10:33 +0000

    backup

--- mi_servidor/main.py
+++ mi_servidor/main.py
@@ -5,5 +5,11 @@ app = FastAPI()
 
 @app.get("/")
 async def root():
-    return {"message": "Hello World"}
+    return {"message": "Hola! servidor de prueba de la clase de Esp. en desarrollo Web!"}
+
+
+
+@app.get("/saludar/{nombre}")
+async def root(nombre:str):
+    return f'Hola, {nombre}, ¿Cómo estás?. \n Este es el servidor de Bruno.'
 
