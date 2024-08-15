05546f83092bb4acb6390267f5ab251bd96c55c4
brunoricciweb <bruno.ricci@bue.edu.ar>
2024-08-15 22:22:08 +0000

    se separa la lógica de la DB del servidor

--- test_db/main.py
+++ test_db/main.py
@@ -1,24 +1,7 @@
 from fastapi import FastAPI
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
-
-class Product(SQLModel, table=True):
-    id: int | None = Field(default=None, primary_key=True)
-    name: str = Field(index=True)
-    description: str
-    price: float | None = Field(default=None, index=True)
-
-
-sqlite_file_name = "database.db"
-sqlite_url = f"sqlite:///{sqlite_file_name}"
-
-connect_args = {"check_same_thread": False}
-engine = create_engine(sqlite_url, echo=True, connect_args=connect_args)
-
-
-def create_db_and_tables():
-    SQLModel.metadata.create_all(engine)
-
+from db import Product, create_db_and_tables, engine
 
 app = FastAPI()
 
